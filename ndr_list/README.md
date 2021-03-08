d44fe6a2147cec2dfaa4688f295b7e6171d90484
Kai Tan <kaitan@Kais-MacBook-Pro.local>
2021-03-08 15:37:08 -0800

    add docker wordpress file and ndr list

---
+++ ndr_list/README.md
@@ -0,0 +1,4 @@
+install cli lib to your local go env first
+- https://github.com/urfave/cli (check)
+- go build/install
+- ndr list
---
+++ ndr_list/ndr.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "info",
+				Value: "package",
+				Usage: "display a nomad package from source",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			files, _ := filepath.Glob("*")
+			for _, num := range files {
+				if strings.HasSuffix(num, ".nomad") {
+					fmt.Println(num)
+				}
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
