521716deac59cc1e848521d87a16e7486d13b54f
rtan <rtan@cn.repomono.com>
2021-03-18 16:33:12 +0800

    combine subcommands in one file

--- ndr/README.md
+++ ndr/README.md
@@ -2,3 +2,19 @@ install cli lib to your local go env first
 - https://github.com/urfave/cli (check)
 - go build/install
 - ndr -install https://raw.githubusercontent.com/UW-Capstone-Nomad/Nomad-Deployment-Recipe/main/test/wordpress.nomad
+
+Subcommands:
+install - 
+pull package and formulae file from urls
+validation/cache
+configuration file setup
+
+uninstall - 
+delete the downloaded package and its dependencies OR/AND
+
+info -
+show the description of the downloaded file
+
+list -
+list downloaded packages names
+
--- ndr/ndr.go
+++ ndr/ndr.go
@@ -6,39 +6,81 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	// curl "github.com/andelf/go-curl"
 	"github.com/urfave/cli/v2"
-	//"https://github.com/urfave/cli/blob/master/docs/v2/manual.md"
+	"strings"
 	"os/exec"
 
 )
 
 func main() {
-	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "install",
-				Value: "package",
-				Usage: "pull a nomad package from source",
+	app := cli.NewApp()
+	app.Name = "Nomad deployment CLI"
+	app.Usage = "Improve users' deployment experience"
+	app.Commands = []*cli.Command{
+		{
+			Name:  "install",
+			// Value: "package",
+			Usage: "pull a nomad package from source",
+			Action: func(c *cli.Context) error {
+				fileUrl := c.String("install")
+				// if c.NArg() > 0 {
+				//   fileUrl = c.Args().Get(0)
+				// }
+				// if c.String("install") == "https://raw.githubusercontent.com/UW-Capstone-Nomad/Nomad-Deployment-Recipe/main/test/wordpress.nomad" {
+				//   DownloadFile("./", fileUrl)
+				//   fmt.Println("Downloading", fileUrl)
+				// } else {
+				//   fmt.Println("Invalid", fileUrl)
+				// }
+				DownloadFile("wordpress.nomad", fileUrl)
+				fmt.Println("Downloading", fileUrl)
+				runFormulae()
+				return nil
+			},		
+		},
+		{
+			Name:  "info",
+			// Value: "package",
+			Usage: "display a nomad package from source",
+			Action: func(c *cli.Context) error {
+				fileUrl := c.String("info")
+				fileUrl += ".nomad"
+				files, _ := filepath.Glob("*")
+				flag := false
+				for _, num := range files {
+					if strings.HasSuffix(num, ".nomad") {
+						if num == fileUrl {
+							fmt.Println(num)
+							fmt.Println("Installed")
+							flag = true
+							break;
+						} 
+							
+					}
+				}
+				if (!flag) {
+					fmt.Println("Do not have this package!")
+				}
+				return nil
 			},
 		},
-		Action: func(c *cli.Context) error {
-			fileUrl := c.String("install")
-			// if c.NArg() > 0 {
-			//   fileUrl = c.Args().Get(0)
-			// }
-			// if c.String("install") == "https://raw.githubusercontent.com/UW-Capstone-Nomad/Nomad-Deployment-Recipe/main/test/wordpress.nomad" {
-			//   DownloadFile("./", fileUrl)
-			//   fmt.Println("Downloading", fileUrl)
-			// } else {
-			//   fmt.Println("Invalid", fileUrl)
-			// }
-			DownloadFile("wordpress.nomad", fileUrl)
-			fmt.Println("Downloading", fileUrl)
-			runFormulae()
-			return nil
-		},		
-	}
+		{
+			Name:  "list",
+			// Value: "package",
+			Usage: "display a nomad package from source",
+			Action: func(c *cli.Context) error {
+				files, _ := filepath.Glob("*")
+				for _, num := range files {
+					if strings.HasSuffix(num, ".nomad") {
+						fmt.Println(num)
+					}
+				}
+				return nil
+			},
+		},
+	  }
 
 	err := app.Run(os.Args)
 	if err != nil {
@@ -48,7 +90,7 @@ func main() {
 
 func runFormulae() error{
 	cmd := exec.Command("sudo", "python3", "/home/rtan/Work_dir/Nomad/ndr/formulae.py")
-	out, err := cmd.Output()
+	_, err := cmd.Output()
 
 	if err != nil {
     	println(err.Error())
