db3625cf406db455e66ae2763a5d127cc93d0eca
linmumu97 <30500624+linmumu97@users.noreply.github.com>
2021-05-08 00:55:03 -0700

    Add log info with "NOTES.txt"

--- backpack/cmd/status.go
+++ backpack/cmd/status.go
@@ -122,25 +122,3 @@ func statusRun(cmd *cobra.Command, args []string) {
         fmt.Println(string(content))
 }
 
-
-func main() {
-	file, err := os.Open("NOTES.txt")
- 
-	if err != nil {
-		log.Fatalf("failed opening file: %s", err)
-	}
- 
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var txtlines []string
- 
-	for scanner.Scan() {
-		txtlines = append(txtlines, scanner.Text())
-	}
- 
-	file.Close()
- 
-	for _, eachline := range txtlines {
-		fmt.Println(eachline)
-	}
-}
